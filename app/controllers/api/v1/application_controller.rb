9318a942129a80648b4d8b8d39d8b733dab4bbd2
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2020-04-04 21:23:54 +0200

    add CRSF

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -4,7 +4,7 @@ module Api
         include DeviseTokenAuth::Concerns::SetUserByToken
         before_action :authenticate_user!
         before_action :configure_permitted_parameters, if: :devise_controller?
-        
+        protect_from_forgery with: :null_session
         protected
         def configure_permitted_parameters
           devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -9,7 +9,7 @@ DeviseTokenAuth.setup do |config|
 
   # By default, users will need to re-authenticate after 2 weeks. This setting
   # determines how long tokens will remain valid after they are issued.
-  # config.token_lifespan = 2.weeks
+  config.token_lifespan = 1.minutes
 
   # Limiting the token_cost to just 4 in testing will increase the performance of
   # your test suite dramatically. The possible cost value is within range from 4
@@ -18,7 +18,7 @@ DeviseTokenAuth.setup do |config|
 
   # Sets the max number of concurrent devices per user, which is 10 by default.
   # After this limit is reached, the oldest tokens will be removed.
-  # config.max_number_of_devices = 10
+  config.max_number_of_devices = 1
 
   # Sometimes it's necessary to make several requests to the API at the same
   # time. In this case, each request in the batch will need to share the same
