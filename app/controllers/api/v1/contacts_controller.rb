4249587938b896df619d072468bb1a5699cdc8d7
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2020-04-11 18:23:02 +0200

    add search to contact && refacto code

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,11 +1,24 @@
 class Api::V1::ContactsController < ApplicationController
-  before_action :set_contact, only: [:show, :update, :destroy]
+  before_action :set_contact, only: %i[show update destroy]
 
   # GET /contacts
   def index
-    @contacts = Contact.order(order_and_direction).page(page).per(per_page).where(roles: params[:role])
+    if params[:search].blank?
+      @contacts = Contact.order(order_and_direction).page(page).per(per_page)
+                         .where(roles: params[:role])
+    else
+      @contacts = Contact.order(order_and_direction).page(page).per(per_page)
+                         .where(["roles = ? and (name like ? or email like ? or
+                         first_phone like ? or second_phone like ? )",
+                                 params[:role], '%' + params[:search] + '%',
+                                 '%' + params[:search] + '%',
+                                 '%' + params[:search] + '%',
+                                 '%' + params[:search] + '%'])
+    end
+
     set_pagination_headers :contacts
-    json_string = ContactSerializer.new(@contacts, include: [:properties]).serialized_json
+    json_string = ContactSerializer.new(@contacts, include: [:properties])
+                                   .serialized_json
     render json: json_string
 
 
@@ -42,13 +55,13 @@ class Api::V1::ContactsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_contact
-      @contact = Contact.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def contact_params
-      params.permit(:name, :first_phone, :email, :second_phone, :roles)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def contact_params
+    params.permit(:name, :first_phone, :email, :second_phone, :roles)
+  end
 end
