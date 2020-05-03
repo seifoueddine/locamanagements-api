d5ee84d00bbd6279b9cff86a0c8d2dcfbf665006
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2020-05-03 05:50:25 +0200

    connecte to actionCable with slug_id

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -11,8 +11,8 @@ module ApplicationCable
       uid = request.query_parameters[:uid]
       token = request.query_parameters[:token]
       client_id = request.query_parameters[:client]
-
-      user = User.find_by_uid(uid)
+      slug_id = request.query_parameters[:slug_id]
+      user = User.where(slug_id: slug_id).find_by_uid(uid)
 
       if user && user.valid_token?(token, client_id)
         user
